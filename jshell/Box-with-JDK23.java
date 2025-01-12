5c85d395139dce48aba8baee7dbdaa65e5af563a
Johan Hutting <9496809+jhutting@users.noreply.github.com>
2025-01-12 17:23:01 +0100

    Update Box-with-JDK23.java

--- jshell/Box-with-JDK23.java
+++ jshell/Box-with-JDK23.java
@@ -4,5 +4,5 @@ record Box(String label, int width, int height, int depth, List<Item> contents) 
 
 Box orig = new Box("example", 210, 297, 100, new ArrayList<>());
 
-// JEP-468, requires --enable-preview and JDK 23
-Box altered = orig with { label="changeme");
+// JEP-468, requires --enable-preview and JDK 23 (although it seems to not have made the cut for 23, 24 yet)
+Box altered = orig with { label = "changeme" };
