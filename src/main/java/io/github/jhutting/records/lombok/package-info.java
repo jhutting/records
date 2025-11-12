4412163b8418448a96bca0c82efdb5ebcfcca29e
jhutting <9496809+jhutting@users.noreply.github.com>
2025-11-12 21:36:30 +0100

    Removed Lombok examples as I couldn't be arsed to fix it for the JDK I'm using.

--- src/main/java/io/github/jhutting/records/lombok/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package was added to display both the migration with OpenRewrite's LombokValueToRecord recipe and the usage of @Builder and @With on records.
- */
-package io.github.jhutting.records.lombok;
